7eddd0df1f8435945a704c2005b0bda38fd4a2e9
Kevin Martensson <kevinmartensson@gmail.com>
2017-08-13 14:19:23 +0200

    Use `replace-buffer` module

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ test(t => {
 	t.is(m('foo\r\nbar\r\nbaz\n'), 'foo\nbar\nbaz\n');
 	t.is(m('foo\nbar\nbaz\r\n'), 'foo\nbar\nbaz\n');
 	t.is(m('foo\nbar\n'), 'foo\nbar\n');
-	t.is(new Buffer(m('foo\r\nbar\r\nbaz\n')).toString(), 'foo\nbar\nbaz\n');
-	t.is(new Buffer(m('foo\nbar\nbaz\r\n')).toString(), 'foo\nbar\nbaz\n');
-	t.is(new Buffer(m('foo\nbar\n')).toString(), 'foo\nbar\n');
+	t.is(m(Buffer.from('foo\r\nbar\r\nbaz\n')).toString(), 'foo\nbar\nbaz\n');
+	t.is(m(Buffer.from('foo\nbar\nbaz\r\n')).toString(), 'foo\nbar\nbaz\n');
+	t.is(m(Buffer.from('foo\nbar\n')).toString(), 'foo\nbar\n');
 	t.throws(m.bind(null, 1), 'Expected a `string` or a `Buffer`, got `number`');
 });
