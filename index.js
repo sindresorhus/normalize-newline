20e3f34f4b5f2bfef6cae6d438f19ae97f4ba35d
Kevin Mårtensson <kevinmartensson@gmail.com>
2017-08-14 23:29:45 +0200

    Use `replace-buffer` module (#4)

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 'use strict';
+const replaceBuffer = require('replace-buffer');
+
 const CRLF = '\r\n';
-
-const replaceBuf = buf => {
-	const i = buf.indexOf(CRLF);
-
-	if (i === -1) {
-		return buf;
-	}
-
-	const start = buf.slice(0, i);
-	const end = replaceBuf(buf.slice(i + CRLF.length));
-	const len = i + start.length + end.length;
-
-	return Buffer.concat([start, '\n', end], len);
-};
 
 module.exports = x => {
 	if (typeof x !== 'string' && !Buffer.isBuffer(x)) {
 		throw new TypeError(`Expected a \`string\` or a \`Buffer\`, got \`${typeof x}\``);
 	}
 
-	return Buffer.isBuffer(x) ? replaceBuf(x) : x.replace(new RegExp(CRLF, 'g'), '\n');
+	return Buffer.isBuffer(x) ? replaceBuffer(x, CRLF, '\n') : x.replace(new RegExp(CRLF, 'g'), '\n');
 };
